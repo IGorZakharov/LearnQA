8c6a6394c2a7c77bd0edc22a0f7c1884be55569b
Zakharov Igor <izakharov@naumen.ru>
2023-08-10 14:43:09 +0500

    get запрос

--- src/test/java/TestPrintln.java
+++ src/test/java/TestPrintln.java
@@ -1,9 +1,19 @@
 import org.junit.jupiter.api.Test;
-
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 public class TestPrintln {
-     @Test
-     public void helloWorld(){
+    @Test
+    public void helloWorld() {
 
-         System.out.println("Hello from Igor");
-     }
+        System.out.println("Hello from Igor");
+    }
+
+    @Test
+    public void getRequest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+
+    }
 }

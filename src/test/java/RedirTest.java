b5e308f85eea4c1e56f688572bc917e6574f64da
Zakharov Igor <izakharov@naumen.ru>
2023-08-18 13:07:06 +0500

    редирект на 200

--- src/test/java/RedirTest.java
+++ src/test/java/RedirTest.java
@@ -1,27 +1,27 @@
 import io.restassured.RestAssured;
-import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class RedirTest {
     @Test
     public void testUrlRedir() {
-       
+
         Response response = RestAssured
                 .given()
                 .redirects()
-                .follow(false)
+                .follow(true)
                 .when()
                 .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
-        response.prettyPrint();
-
-        String responseLocation = response.getHeader("Location");
-        System.out.println(responseLocation);
+        int statusCode = response.getStatusCode();
+        for (int i = 0; i == 200;)
+             { RestAssured.given().redirects().follow(true);
+                 System.out.println(statusCode);
+        }
+        System.out.println(statusCode);
+    }
 
 
     }
-}
+
